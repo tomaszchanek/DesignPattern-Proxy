5e3391d9574f207e833ebcfd5269aa71e0e3908c
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-07-10 15:19:36 +0200

    feat: implement Proxy design pattern

---
+++ src/proxy/YouTubeCacheProxy.java
@@ -0,0 +1,44 @@
+package proxy;
+
+import some_cool_media_library.ThirdPartyYouTubeClass;
+import some_cool_media_library.ThirdPartyYouTubeLib;
+import some_cool_media_library.Video;
+
+import java.util.HashMap;
+
+public class YouTubeCacheProxy implements ThirdPartyYouTubeLib {
+    private ThirdPartyYouTubeLib youTubeService;
+    private HashMap<String, Video> cachePopular = new HashMap<String, Video>();
+    private HashMap<String, Video> cacheAll = new HashMap<String, Video>();
+
+    public YouTubeCacheProxy() {
+        this.youTubeService = new ThirdPartyYouTubeClass();
+    }
+
+    @Override
+    public HashMap<String, Video> popularVideos() {
+        if (cachePopular.isEmpty()) {
+            cachePopular = youTubeService.popularVideos();
+        } else {
+            System.out.println("Retrieved from cache");
+        }
+        return cachePopular;
+    }
+
+    @Override
+    public Video getVideo(String videoId) {
+        Video video = cacheAll.get(videoId);
+        if (video == null) {
+            video = youTubeService.getVideo(videoId);
+            cacheAll.put(videoId, video);
+        } else {
+            System.out.println("Retrieved video '" + videoId + "' from cache.");
+        }
+        return video;
+    }
+
+    public void reset() {
+        cachePopular.clear();
+        cacheAll.clear();
+    }
+}
